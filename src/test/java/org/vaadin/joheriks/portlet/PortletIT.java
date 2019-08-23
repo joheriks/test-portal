fafbb3ecbfa010313fe76c87f788b7945f54a69c
Johannes Eriksson <joheriks@vaadin.com>
2019-08-23 14:28:48 +0300

    Basic integration test

---
+++ src/test/java/org/vaadin/joheriks/portlet/PortletIT.java
@@ -0,0 +1,19 @@
+package org.vaadin.joheriks.portlet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.flow.component.notification.testbench.NotificationElement;
+
+public class PortletIT extends AbstractPlutoPortalTest {
+
+    @Test
+    public void clickingButtonShowsNotification() {
+        Assert.assertFalse($(NotificationElement.class).exists());
+
+        $(ButtonElement.class).first().click();
+
+        Assert.assertTrue($(NotificationElement.class).waitForFirst().isOpen());
+    }
+}
